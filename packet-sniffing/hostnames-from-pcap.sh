d92d230cd1c2dd145abb0ed306a0470ba9f7611c
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2019-07-08 23:54:27 -0400

    finished script

--- packet-sniffing/hostnames-from-pcap.sh
+++ packet-sniffing/hostnames-from-pcap.sh
@@ -1,11 +1,23 @@
 #! /bin/sh
+# TODO: Abstract script
 
-#tshark -r set2.pcap -2 -T fields -R ip -e ip.dst \
-#| sort -n | uniq -c | sort -n > sorted-ips.txt
+tshark -r set2.pcap -2 -T fields -R ip -e ip.dst \
+| sort -n | uniq -c | sort -n > sorted-ips.txt
 
+# Command tells tshark to read from the set2.pcap file using a
+# two-pass analysis (-2) and to format the output into fields.
+# "-R ip" tells tshark to apply a filter to isolate IP addresses.
+# the -e are to define the fields to be displayed. The results
+# are then piped to unix commands that first sort the IPs 
+# numerically, remove duplicate rows while prepending a count of
+# the duplicates, then to sort the addresses numerically based
+# on the number of duplicates. All the results are printed to
+# the file "sorted-ips".
+
+# Removes duplicate count and saves to an array of IP addresses
 IPS=$(cat sorted-ips.txt | awk '{print $2}')
 
-
+# Reverse DNS lookup every IP address in the array
 for i in $IPS; do
-        echo $i
+        dig +noall +answer -x $i
 done
