6f87f28a3e7988f898ef0a935e65ed06ab7ecd48
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2019-07-08 23:39:57 -0400

    testing file to array

--- packet-sniffing/hostnames-from-pcap.sh
+++ packet-sniffing/hostnames-from-pcap.sh
@@ -1,1 +1,11 @@
 #! /bin/sh
+
+#tshark -r set2.pcap -2 -T fields -R ip -e ip.dst \
+#| sort -n | uniq -c | sort -n > sorted-ips.txt
+
+IPS=$(cat sorted-ips.txt | awk '{print $2}')
+
+
+for i in $IPS; do
+        echo $i
+done
